82e383fb9ee1345e7f30c6ab1974af7bb44a59a5
MICHAEL KINFE <akikinfe@gmail.com>
2022-07-28 13:09:51 -0500

    animation

---
+++ quiz/style.js
@@ -0,0 +1,14 @@
+$ (document).ready(function(){
+ $("body").html("<div><P> Hello world</p></div>")
+    // $("div li").css({"background-color": "gray"});
+    
+    //  $("p.first").each(function (){
+    //     $(this).css('background-color', 'red')
+    //  });
+    
+    //  $('#myid').each(function(idx){
+    //     $(this).css('background-color','blue')
+    //  });
+});
+
+ 
