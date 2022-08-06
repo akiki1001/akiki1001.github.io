eb789f91300d08e534aa2f23b629e769cbffcb28
MICHAEL KINFE <akikinfe@gmail.com>
2022-08-06 02:46:42 -0500

    astronomy picture

---
+++ AstronomyPicture1/public/apod.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+  $("#view_button").click(getPicture);
+  console.log('apod');
+});
+function getPicture() {
+  $.ajax({
+    url: "https://api.nasa.gov/planetary/apod",
+    type: "GET",
+    data: { api_key: "gyhaJ2GQ5PF58E8w6iESVvscg2en3nrGcOqczI2s", date: $("#date").val() },
+    dataType: "json",
+    success: showPicture,
+    error: noPicture,
+  });
+console.log('getpic');
+  // $.get("https://api.nasa.gov/planetary/apod", {
+  //   data: { api_key: "gyhaJ2GQ5PF58E8w6iESVvscg2en3nrGcOqczI2s", date: $("#date").val() },
+  //   dataType: "json",
+  //   success: showPicture,
+  //   error: noPicture,
+  // });
+}
+function showPicture(data) {
+  $("#pic").attr("src", data.url);
+}
+function noPicture(error) {
+  alert(error.responseText);
+}
